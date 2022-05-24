da325459a8de3b3db2adaf828fea6403c9e0244a
zvonimirbrisevac <zb51687@fer.hr>
2022-05-25 01:11:58 +0200

    Successfully resolving demo ip address

---
+++ src/test/kotlin/com/mkt/ipaddressresolver/IpAddressResolverApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mkt.ipaddressresolver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class IpAddressResolverApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
